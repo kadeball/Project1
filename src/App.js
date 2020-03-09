4d8aa64e8f245c7d668b9d426fa5acd48d2b269c
kadeball <kadeball@iu.edu>
2020-03-09 18:08:25 -0500

    my head hurts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Wrapper from "./components/Wrapper";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Clickfunko from "./components/Clickfunko";
+
 
 function App() {
+
+    const [category, setCategory] = React.useState('all');
+
+
+    function handleClick( category ) {
+        setCategory(category);
+
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+          <div className="App">
+              <Nav clickHandler={handleClick}/>
+              <Wrapper category={category}/>
+              <Switch>
+              <Route exact path="/" component={Wrapper}/>
+              <Route path="/:name" component={Clickfunko}/>
+              </Switch>
+          </div>
+      </Router>
+
+
+
   );
 }
 
